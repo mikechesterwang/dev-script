e17ea9e56dc121aa5b5e6af8f6e187bdbe642563
Mike Chester Wang <52522981+mikechesterwang@users.noreply.github.com>
2022-05-18 10:40:08 +0800

    Create go-1.18-amd64.sh

---
+++ go-1.18-amd64.sh
@@ -0,0 +1,4 @@
+curl -O https://dl.google.com/go/go1.18.2.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
