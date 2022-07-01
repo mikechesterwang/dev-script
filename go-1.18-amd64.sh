5b5e93879d03a2642507d6195b29b23ccf536c32
Mike Chester Wang <52522981+mikechesterwang@users.noreply.github.com>
2022-07-01 12:11:21 +0800

    Update go-1.18-amd64.sh

--- go-1.18-amd64.sh
+++ go-1.18-amd64.sh
@@ -1,5 +1,5 @@
 curl -O https://dl.google.com/go/go1.18.2.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
+echo "PATH=/usr/local/go/bin:\$PATH" >> $HOME/.profile
+source $HOME/.profile
 go version
